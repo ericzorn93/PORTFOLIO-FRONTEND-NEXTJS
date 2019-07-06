d5b8ac31215c1940fb56df98ec7c00987e58bce5
Eric Zorn <zornwebdev@gmail.com>
2019-07-06 18:25:33 -0400

    Redux dev tools and store setup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,19 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+
 import rootReducer from "./reducers";
 
 const middleware = [thunk];
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const composeEnhancers = composeWithDevTools({});
 
+// Determine if devtools need to be present, based on the node environment
+let loadMiddleware: any;
+if (process.env.NODE_ENV !== "production") {
+  loadMiddleware = composeEnhancers(applyMiddleware(...middleware));
+} else {
+  loadMiddleware = applyMiddleware(...middleware);
+}
+
+const store = createStore(rootReducer, loadMiddleware);
 export default store;
