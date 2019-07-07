b2f7efcc7e8751e757aac5e8f73ecb7fe369d194
Eric Zorn <zornwebdev@gmail.com>
2019-07-07 11:12:35 -0400

    Added appllo in memory cache and apollo link http

--- src/apollo_setup/index.ts
+++ src/apollo_setup/index.ts
@@ -1,7 +1,13 @@
 import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+// import { HttpLink } from "apollo-link-http";
+
+const cache: InMemoryCache = new InMemoryCache();
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  // link: new HttpLink(),
+  uri: "http://localhost:4000/graphql",
+  cache
 });
 
 export default apolloClient;
