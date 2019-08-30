3d98665d031206e512b118dd35f2edfd7e054d2e
Eric Zorn <zornwebdev@gmail.com>
2019-08-29 22:05:33 -0400

    Formatting

--- setup/wrapWithProviders.tsx
+++ setup/wrapWithProviders.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import {ApolloProvider} from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 
 import assembleStore from "../src/store";
 import apolloClient from "../src/apollo/apolloClient";
