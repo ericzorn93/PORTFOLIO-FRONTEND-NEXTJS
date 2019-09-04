d35302a76193cd85fe9794a1e81826ebc3b9edfe
ericzorn93 <zornwebdev@gmail.com>
2019-09-03 21:29:49 -0400

    Body is using display grid

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -6,7 +6,8 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from '../Header/header';
@@ -32,27 +33,25 @@ const Layout = ({ children }: ILayout) => {
   `);
 
   return (
-    <>
-      <Header siteTitle={siteTitle} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <React.Fragment>
+      <div css={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+        <Header siteTitle={siteTitle} />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+        </div>
       </div>
-    </>
+      <footer>
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </React.Fragment>
   );
-};
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
