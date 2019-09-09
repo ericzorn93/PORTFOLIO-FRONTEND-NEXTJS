24d1fa5ff48d6bf4b55dcfd2a6b7177c142d9534
Eric Zorn <ericzorndesigns@gmail.com>
2019-09-09 07:49:57 -0400

    Begin adding contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '../components/Layout/layout';
+
+const Contact = () => {
+  return (
+    <>
+      <Layout>
+        <p>Contact Page</p>
+      </Layout>
+    </>
+  );
+};
+
+export default Contact;
