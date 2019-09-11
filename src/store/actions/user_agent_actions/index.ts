c0610fab9118254463693b6aa2aa911d7cc161a4
Eric Zorn <ericzorndesigns@gmail.com>
2019-09-11 07:48:31 -0400

    Add further comments for the user agent redux actions

--- src/store/actions/user_agent_actions/index.ts
+++ src/store/actions/user_agent_actions/index.ts
@@ -1,7 +1,7 @@
 class UserAgentActions {
   /**
    * @description
-   * Load initial user agent data to the redux store
+   * Load initial user agent data to the redux store and determine if user agent isDevice (mobile)
    */
   public static loadInitialUserAgent(): Function {
     return dispatch => {};
