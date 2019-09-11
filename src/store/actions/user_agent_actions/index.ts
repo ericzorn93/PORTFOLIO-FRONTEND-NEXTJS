7d74f2aeade9a67703c2c07c805e49aeb72a7890
Eric Zorn <ericzorndesigns@gmail.com>
2019-09-10 22:24:32 -0400

    Begin adding user agent actions and static method in UserAgentActions class

---
+++ src/store/actions/user_agent_actions/index.ts
@@ -0,0 +1,9 @@
+class UserAgentActions {
+  /**
+   * @description
+   * Load initial user agent data to the redux store
+   */
+  public static loadInitialUserAgent(): Function {
+    return dispatch => {};
+  }
+}
