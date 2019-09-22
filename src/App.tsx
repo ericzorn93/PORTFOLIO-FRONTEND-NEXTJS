c80e9a775cdfdd7b6aac3fcc3931d38e8bdf1c3e
Eric Zorn <zornwebdev@gmail.com>
2019-09-22 19:42:43 -0400

    Convert theme logic into the App.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
+import { useSelector, useDispatch } from 'react-redux';
 
 import './App.css';
 import { CustomRoutes } from './router/routes';
+import { useAllThemesQuery } from './generated/graphql';
+import { ThemeActions } from './store/actions/theme_actions';
 
 const App: React.FC = () => {
+  /** Beginning of Redux */
+  const allThemes = useSelector((state: any) => state.themes.allThemes);
+  const dispatch = useDispatch();
+  /** End of Redux */
+
+  /** Beginning of GraphQL Queries */
+  const {
+    loading: themeLoading,
+    data: themeData,
+    error: themeError,
+  } = useAllThemesQuery();
+  /** End of GraphQL Queries */
+
+  if (
+    !Object.values(allThemes.darkMode).length &&
+    !Object.values(allThemes.lightMode).length &&
+    themeData
+  ) {
+    const { darkMode, lightMode } = themeData.allThemes;
+
+    const themes = {
+      darkMode,
+      lightMode,
+    };
+
+    dispatch(ThemeActions.addAllThemeDataAction(themes));
+  }
+
+  if (themeError || themeLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={allThemes}>
       <div className="App">
         <CustomRoutes />
       </div>
--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 
@@ -7,42 +6,8 @@ import './styles/layout.css';
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
 import { PageWrapper } from './styles/layout.styles';
-import { useAllThemesQuery } from '../../generated/graphql';
-import { ThemeActions } from '../../store/actions/theme_actions';
 
 const Layout = ({ children }) => {
-  /** Beginning of Redux */
-  const allThemes = useSelector((state: any) => state.themes.allThemes);
-  const dispatch = useDispatch();
-  /** End of Redux */
-
-  /** Beginning of GraphQL Queries */
-  const {
-    loading: themeLoading,
-    data: themeData,
-    error: themeError,
-  } = useAllThemesQuery();
-  /** End of GraphQL Queries */
-
-  if (
-    !Object.values(allThemes.darkMode).length &&
-    !Object.values(allThemes.lightMode).length &&
-    themeData
-  ) {
-    const { darkMode, lightMode } = themeData.allThemes;
-
-    const themes = {
-      darkMode,
-      lightMode,
-    };
-
-    dispatch(ThemeActions.addAllThemeDataAction(themes));
-  }
-
-  if (themeError || themeLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <React.Fragment>
       <PageWrapper>
