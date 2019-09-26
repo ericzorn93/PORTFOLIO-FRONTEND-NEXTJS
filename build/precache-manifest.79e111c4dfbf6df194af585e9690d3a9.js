8a04d6f6e875b6d5e4aa73154d95b2775048ecb8
Eric Zorn <zornwebdev@gmail.com>
2019-09-25 21:26:05 -0400

    New production build with generated company and company genre query hooks, HOCs, and components with render props

---
+++ build/precache-manifest.79e111c4dfbf6df194af585e9690d3a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ec079aaf3d8ed6d82f9eb814950487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a703d08504dadce54c9",
+    "url": "/static/css/main.61e9a2d8.chunk.css"
+  },
+  {
+    "revision": "3a571a794150cbc4625e",
+    "url": "/static/js/2.4aba8545.chunk.js"
+  },
+  {
+    "revision": "8a703d08504dadce54c9",
+    "url": "/static/js/main.25408efe.chunk.js"
+  },
+  {
+    "revision": "ded2a2633592fcb02f60",
+    "url": "/static/js/runtime-main.fa7ff26f.js"
+  }
+]);
