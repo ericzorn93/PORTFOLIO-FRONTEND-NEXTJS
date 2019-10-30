8a0e528830a61f763df13af05536ba6a2eb3ae2e
Eric Zorn <zornwebdev@gmail.com>
2019-10-29 20:43:34 -0400

    Redux is currently working with the application

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,4 @@
  */
 
 // You can delete this file if you're not using it
-export {
-    default as wrapRootElement
-}
-from './src/state/ReduxWrapper';
+export { default as wrapRootElement } from "./src/store/ReduxWrapper";
