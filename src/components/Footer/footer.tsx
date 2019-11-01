b884d03291cb310dba424bdf426bab1e55cad204
Anthony Fox <anthonysfox1@gmail.com>
2019-10-31 23:00:23 -0400

    Testing

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -11,6 +11,7 @@ const Footer: React.FC<IProps> = ({ name }) => (
   <FooterWrapper>
     <div>
       <h1>{name}</h1>
+      <h1>hello</h1>
       &copy; All rights are reserved | {new Date().getFullYear()}
     </div>
   </FooterWrapper>
