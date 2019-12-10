54a405ec5cbd669efc7f2ba3cefe39852b8cae80
Eric Zorn <zornwebdev@gmail.com>
2019-12-10 11:23:46 -0500

    Further animation and position updating

--- src/components/styled/PositionCenter.ts
+++ src/components/styled/PositionCenter.ts
@@ -1,23 +1,47 @@
 import styled from '@emotion/styled';
 
 export const PositionCenter = styled.div`
-  width: 100%;
-  height: 100vh;
+  --transition-text: all 0.2s ease-in-out;
+  --transform-text: scale(1.1);
+
+  width: 100vw;
   padding: 100px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  z-index: -1;
 
   .personName {
     font-size: 60px;
     font-weight: 300;
+    transition: var(--transition-text);
+
+    &:hover {
+      transform: var(--transform-text);
+    }
   }
 
   .jobTitle {
     font-size: 62px;
     font-weight: 800;
+    transition: var(--transition-text);
+
+    &:hover {
+      transform: var(--transform-text);
+    }
   }
 
   .description {
     font-weight: 400;
     font-size: 24px;
+    transition: var(--transition-text);
+
+    &:hover {
+      transform: var(--transform-text);
+    }
   }
 
   @media (max-width: 800px) {
