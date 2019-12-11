d9f2987e6175d2c3f172da83f44d3e698f91264b
Eric Zorn <zornwebdev@gmail.com>
2019-12-11 13:41:13 -0500

    Rebuilt

---
+++ public/component---src-templates-github-project-template-tsx-c5928b3223c2ee198dd9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{sVHL:function(t,e,n){"use strict";n.r(e);n("f3/d"),n("q1tI");var b=n("qKvR");e.default=function(t){var e=t.path,n=t.pageContext.project;return Object(b.b)("div",null,Object(b.b)("h1",{style:{color:"green"}},n.name),Object(b.b)("h4",null,Object(b.b)("em",null,"Path: ",e)),Object(b.b)("p",null,n.about))}}}]);
+//# sourceMappingURL=component---src-templates-github-project-template-tsx-c5928b3223c2ee198dd9.js.map
---
+++ public/styles-4a0bf1c3c453f2a90b76.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{d1Pv:function(n,o,w){},ncIN:function(n,o,w){}}]);
+//# sourceMappingURL=styles-4a0bf1c3c453f2a90b76.js.map
