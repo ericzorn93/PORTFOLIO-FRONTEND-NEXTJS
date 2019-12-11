835df312af43d86c0965ce782e2e643a6c8f9fc4
Eric Zorn <zornwebdev@gmail.com>
2019-12-11 14:07:26 -0500

    Adding blog page, contact page, and projects page, all without content

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout/layout';
+
+const ContactPage: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Contact Page</h1>
+    </Layout>
+  );
+};
+
+export default ContactPage;
---
+++ src/pages/projects.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout/layout';
+
+const ProjectsPage: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Projects Page</h1>
+    </Layout>
+  );
+};
+
+export default ProjectsPage;
