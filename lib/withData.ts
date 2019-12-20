18aa1e355f528e286a8498b694d2e7cbb087d147
Eric Zorn <zornwebdev@gmail.com>
2019-12-20 12:29:55 -0500

    New github token and with data update

--- lib/withData.ts
+++ lib/withData.ts
@@ -5,7 +5,6 @@ import { PrimaryUtils } from "../shared/utils/primary.utils";
 
 function createClient(serverData: any): ApolloClient<any> {
   // Token for GitHub URL
-  const GITHUB_TOKEN = "6792c14de6f48f0c3113fbbb51c250b0ed793919";
 
   const portfolioLink = new HttpLink({
     uri: PrimaryUtils.graphQlUrl,
@@ -15,7 +14,7 @@ function createClient(serverData: any): ApolloClient<any> {
   const githubLink = new HttpLink({
     uri: "https://api.github.com/graphql",
     headers: {
-      Authorization: `Bearer ${GITHUB_TOKEN}`
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
     }
   });
 
--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
-const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
-const withSass = require("@zeit/next-sass");
+// Initialize doteenv library
+require("dotenv").config();
 
-module.exports = withSass({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+module.exports = {
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: "empty"
+    };
+    /**
+     * Returns environment variables as an object
+     */
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
 
+    /** Allows you to create global constants which can be configured
+     * at compile time, which in our case is our environment variables
+     */
+    config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   }
-});
+};
