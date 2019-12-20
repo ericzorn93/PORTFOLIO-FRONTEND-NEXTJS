5df05665c769f1ed0a171de132060a0cb8951dc5
Eric Zorn <zornwebdev@gmail.com>
2019-12-20 13:11:47 -0500

    Beginning the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,11 +4,45 @@ import { jsx } from "@emotion/core";
 import { NextComponentType } from "next";
 
 import { PageContainer } from "../components/page_styles/overall/overall";
+import { Paragraph } from "../components/primary/elements/paragraph";
+import { ProfilePicture } from "../styles/styled_components/profile_picture";
+import { PageTitle } from "../styles/styled_components/page_title";
 
 const AboutPage: NextComponentType = ({ themeData }: any) => {
   return (
     <PageContainer>
-      <h1>About Page</h1>
+      <PageTitle>About Me</PageTitle>
+      <div css={{ display: "flex" }}>
+        <div
+          css={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column"
+          }}
+        >
+          <ProfilePicture
+            src="/images/ProfilePicture.jpg"
+            alt="Profile Picture"
+          />
+          <Paragraph css={{ marginTop: 10 }}>
+            <b>Eric Zorn</b>
+          </Paragraph>
+        </div>
+
+        <div>
+          <Paragraph css={{ margin: 40, width: "80%" }}>
+            I am a passionate developer, specializing in the MERN stack, with a
+            solid knowledge of SQL-based ORM solutions as well. I deeply enjoy
+            using React with Redux for global state management, TypeScript,
+            Express, Node JS, and recently Apollo and GraphQL. Also, I have been
+            experimenting with React Native, pulling upon my React skillset. I
+            enjoy building scalable web applications with a strong front-end and
+            back-end architecture, utilizing all of the most recent development
+            technologies.
+          </Paragraph>
+        </div>
+      </div>
     </PageContainer>
   );
 };
---
+++ styles/styled_components/profile_picture.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const ProfilePicture = styled.img`
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+`;
